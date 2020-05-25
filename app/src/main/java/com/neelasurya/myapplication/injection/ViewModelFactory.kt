cb70e7bd294072b35f690c2053ac99b2148518fb
soumya Chaudhary <>
2020-05-25 20:23:36 +0530

    Offline support

--- app/src/main/java/com/neelasurya/myapplication/injection/ViewModelFactory.kt
+++ app/src/main/java/com/neelasurya/myapplication/injection/ViewModelFactory.kt
@@ -4,20 +4,28 @@ package com.neelasurya.myapplication.injection
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.neelasurya.myapplication.feature.post.PostListViewModel
-
 
 
 class ViewModelFactory(private val activity: AppCompatActivity) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(PostListViewModel::class.java)) {
             // Write a message to the database
-            val database = Firebase.database
-            val reference = database.reference
+            var mDatabase: DatabaseReference
+            try {
+                val database = FirebaseDatabase.getInstance()
+                database.setPersistenceEnabled(true)
+                mDatabase = database.reference
+                mDatabase.keepSynced(true)
+            } catch (e: Exception) {
+                val database = FirebaseDatabase.getInstance()
+                mDatabase = database.reference
+            }
             @Suppress("UNCHECKED_CAST")
-            return PostListViewModel(reference) as T
+            return PostListViewModel(mDatabase) as T
+
         }
         throw IllegalArgumentException("Unknown ViewModel class")
 
